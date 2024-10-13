b8c040be1cad997407336ca1e11a914d12fd6b45
Eryk Szczesniak <64661383+erykszczesniak@users.noreply.github.com>
2024-10-13 14:11:25 +0200

    Delete react-blog-frontend/react-blog-frotend directory

--- react-blog-frontend/react-blog-frotend/src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const getData = async () => {
-  const url = 'http://localhost:8080/api/posts/';
-  const { data : { data : { posts } } } = await axios.get(url);
-  return posts;
-}
